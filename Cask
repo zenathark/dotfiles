f94887ca0e1517a68767e771bb3dde9f502eea7b
Juan Carlos Galan Hernandez <jcgalanh@gmail.com>
2014-12-10 11:17:09 -0600

    Added evil mode (basic) and fixed the f11 keybinding to fullscreen mode

--- Cask
+++ Cask
@@ -1,3 +1,5 @@
 (source melpa)
 
+(depends-on "ergoemacs-mode")
+(depends-on "evil")
 (depends-on "pallet")
--- init.el
+++ init.el
@@ -2,3 +2,30 @@
 (package-initialize)
 (require 'cask)
 (cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+
+;;; Start ergo-emacs (yes i fear pinky emacs)
+(require 'ergoemacs-mode)
+(setq ergoemacs-theme nil)
+(setq ergoemacs-keyboard-layout "us")
+(global-set-key (kbd "<f11>") 'toggle-frame-fullscreen)
+
+;;; More sins, i like vim style
+(require 'evil)
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode t)
+ '(org-CUA-compatible nil)
+ '(recentf-mode t)
+ '(shift-select-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
